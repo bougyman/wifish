85323fea1a2b0a3dfab59b6e75e8afd53a2907a6
bougyman <bougyman@rubyists.com>
2015-03-27 13:21:47 -0500

    tests working again with menu added

--- test/test.sh
+++ test/test.sh
@@ -2,7 +2,7 @@
 errors=0
 errmsgs=""
 test_int=Wifi
-wscan_lines=24
+wscan_lines=23
 waiting=" * Scanning For APs ..."
 heading="SSID[[:space:]]+SIGNAL[[:space:]]+MAC[[:space:]]+FREQ[[:space:]]+CAPABILITIES"
 tests=0
@@ -18,15 +18,22 @@ add_test() {
 	tests=$((tests+1))
 }
 
-tfile=$(mktemp /tmp/$$.XXX.parsed)
-trap 'rm -f $tfile' INT TERM EXIT
+add_test
+bashisms=$(checkbashisms -p wifish 2>&1)
+if [ $? -ne 0 ];then
+	add_err "$bashisms"
+fi
 
-AWK_LOCATION=./awk _IN_TEST=true ./wifish > $tfile
+tfile=$(mktemp /tmp/$$.XXX.parsed)
+efile=$(mktemp /tmp/$$.XXX.parsed.err)
+trap 'rm -f $tfile' INT TERM EXIT
+trap 'rm -f $efile' INT TERM EXIT
+AWK_LOCATION=./awk _IN_TEST=true ./wifish >$tfile 2>$efile
 
 lcount=$(cat $tfile|wc -l)
 add_test
 if [ $lcount -lt 2 ];then
-	add_err "No Output from wifish! At line 17"
+	add_err "No Output from wifish! At line 36"
 fi
 
 add_test
@@ -34,19 +41,19 @@ if [ $lcount -ne $wscan_lines ];then
 	add_err "Expected wifish to produce $wscan_lines lines, got $lcount"
 fi
 
-waitmsg="$(head -1 $tfile)"
+waitmsg="$(head -1 $efile)"
 add_test
 if [ "$waitmsg" != "$waiting" ];then
 	add_err "Expected Wait Message to be '${waiting}', got '${waitmsg}'"
 fi
 
-interface="$(head -2 $tfile|tail -1|awk '{print $(NF)}')"
+interface="$(head -1 $tfile|awk '{print $(NF)}')"
 add_test
 if [ "$interface" != "'$test_int'" ];then
 	add_err "Expected interface to be '$test_int', got '$interface'"
 fi
 
-header="$(head -3 $tfile|tail -1)"
+header="$(head -2 $tfile|tail -1)"
 echo "$header"|egrep -q "$heading"
 add_test
 if [ $? -ne 0 ];then
